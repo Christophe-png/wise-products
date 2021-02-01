1269f050ec2aec5c37487ad1e04f6790d98eb8a7
CASTAN <christophecastan4@gmail.com>
2021-02-01 18:03:46 +0100

    create header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="logo"
+        src="https://cdn.shopify.com/s/files/1/0266/5526/0732/files/Logo_pour_Facebook_lien_site_600x600.png?v=1580466220"
+        alt="logo Wise Trail running"
+      />
+    </div>
+  );
+}
+
+export default Header;

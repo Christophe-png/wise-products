83ed258d4438f1a9aeeb4377acc534da83274879
CASTAN <christophecastan4@gmail.com>
2021-02-01 18:51:53 +0100

    add link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
 import Header from "./Components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header className="header"></Header>
+      <Router>
+        <Header className="header"></Header>
+      </Router>
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -9,6 +10,28 @@ function Header() {
         src="https://cdn.shopify.com/s/files/1/0266/5526/0732/files/Logo_pour_Facebook_lien_site_600x600.png?v=1580466220"
         alt="logo Wise Trail running"
       />
+
+      <Link className="nav-homepage" to="/">
+        <h1 className="title">Wise</h1>
+      </Link>
+
+      <ul>
+        <li>
+          <Link className="nav-wise-products" to="/product">
+            Boutique
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-a-propos" to="/a-propos">
+            A propos
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-contact-form" to="/contact-form">
+            Nous contacter
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }

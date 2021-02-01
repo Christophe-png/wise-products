1a19978174cf1b5a316c59659927b90c79cb2d8d
CASTAN <christophecastan4@gmail.com>
2021-02-01 18:07:52 +0100

    modif error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="header"></header>
+      <Header className="header"></Header>
     </div>
   );
 }
